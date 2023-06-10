f7d326ae7d8f5fae86983de904caba745eb54440
Ruslan <kovalyovruslan@gmail.com>
2023-06-10 06:02:02 +0300

    page 404

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('404/', views.page_404, name='404'), # 404 page for testing. Hendler404 in restaurant_carnebar/url.py
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def handler404(request, exception):
+    return render(request, 'main/404.html', status=404)
+    
+def page_404(request):
+    return render(request, 'main/404.html')
