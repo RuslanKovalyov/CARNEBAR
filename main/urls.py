47a8cb5bd9d8c73d5e55a55875b0e94ced019e79
Ruslan <kovalyovruslan@gmail.com>
2023-06-13 11:34:00 +0300

    gallery page with img displayer script

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.dev, name='dev'),
     path('home/', views.index, name='home'),
     path('contacts', views.contacts, name='contacts'),
+    path('gallery', views.gallery, name='gallery'),
     path('404/', views.page_404, name='404'), # 404 page for testing. Hendler404 in restaurant_carnebar/url.py
 ]
--- main/views.py
+++ main/views.py
@@ -9,6 +9,9 @@ def index(request):
 def contacts(request):
     return render(request, 'main/contacts.html')
 
+def gallery(request):
+    return render(request, 'main/gallery.html')
+
 def handler404(request, exception):
     return render(request, 'main/404.html', status=404)
     
