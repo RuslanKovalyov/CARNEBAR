36271a5bcd7d5ca0061cd1123d57084e914c82a3
Ruslan <kovalyovruslan@gmail.com>
2023-06-14 17:01:08 +0300

    debag mode

--- restaurant_carnebar/settings.py
+++ restaurant_carnebar/settings.py
@@ -13,7 +13,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'carnebar.co.il', 'www.carnebar.co.il']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://carnebar.co.il', 'https://www.carnebar.co.il']
@@ -110,9 +110,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "main/static"),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+if DEBUG:
+    STATIC_URL = '/static/'
+    # STATICFILES_DIRS = [os.path.join(BASE_DIR, "main/static"),]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+else:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = os.environ['AWS_S3_CUSTOM_DOMAIN']
+
+    # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',#1 day
+    # 'ContentEncoding': 'gzip'
+    }
+
+    AWS_LOCATION = os.environ['AWS_LOCATION']
+    AWS_STATIC_LOCATION = f'{AWS_LOCATION}/static'
+    AWS_MEDIA_LOCATION = f'{AWS_LOCATION}/media'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
