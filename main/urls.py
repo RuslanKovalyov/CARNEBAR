458591e1497c824086d7913464c4a19027a7cf25
Ruslan <kovalyovruslan@gmail.com>
2023-06-14 17:06:08 +0300

    home url to prodaction page

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dev, name='dev'),
-    path('home/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('contacts', views.contacts, name='contacts'),
     path('gallery', views.gallery, name='gallery'),
+    path('dev', views.dev, name='dev'),
     path('404/', views.page_404, name='404'), # 404 page for testing. Hendler404 in restaurant_carnebar/url.py
 ]
